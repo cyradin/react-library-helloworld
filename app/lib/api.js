0959316c836cc970453706a545c194848f3ab701
cyradin <cyradin@nextmail.ru>
2016-11-16 18:16:52 +0300

    removed memory-cache

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,5 +1,4 @@
 import request from 'browser-request';
-import cache from 'memory-cache';
 
 const API_VERSION = 'v1';
 
@@ -7,7 +6,7 @@ var url = {
     books: '/api/' + API_VERSION + '/books'
 }
 
-var defaults = { method: 'GET', json: true, cache: true, cacheTime: 15000 };
+var defaults = { method: 'GET', json: true };
 
 function isSuccessful (response, data) {
     return response.status < 400 && data.success;
@@ -16,23 +15,8 @@ function isSuccessful (response, data) {
 function sendRequest (options, callback) {
     options = Object.assign({}, defaults, options);
 
-    var useCache = options.cache,
-        cacheTime = options.cacheTime,
-        data;
-
-    if (useCache && (data = cache.get(options.url))) {
-        callback(data);
-        return;
-    }
-
-    delete(options.cache);
-    delete(options.cacheTime);
-
     request(options, function(err, response, data) {
         if (! err && isSuccessful(response, data)) {
-            if (useCache) {
-                cache.put(options.url, data.data, cacheTime);
-            }
             callback(data.data);
         }
     });
@@ -47,10 +31,10 @@ var api = {
             sendRequest({ url: url.books + '/' + id }, callback);
         },
         edit: function (data) {
-            sendRequest({ url: url.books + '/' + data.id + '/edit', json: data, cache: false }, callback);
+            sendRequest({ url: url.books + '/' + data.id + '/edit', json: data, method: 'POST' }, callback);
         },
         add: function () {
-            sendRequest({ url: url.books + '/add', json: data, cache: false }, callback);
+            sendRequest({ url: url.books + '/add', json: data, method: 'POST' }, callback);
         }
     }
 }
